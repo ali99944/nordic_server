1fad0ef5646377445ad5eb1d7f95539fdeac1d0f
ali99944 <ali99944>
2023-09-15 07:08:47 +0300

    upload

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.post('/api/notifications/devices', async (req,res) =>{
               title: 'Your Title XD',
               body: 'Your Message',
               type: 'device',
-              imei:req.body.imeis
+              imei:JSON.stringify(req.body.imeis)
             },
             topic: 'nordic', // Replace with the topic you want to use
           };
