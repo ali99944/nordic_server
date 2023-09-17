7da3899867fd1af1611c9d7876b4400a29f62cfb
ali99944 <ali99944>
2023-09-17 21:09:57 +0300

    upload

--- models/Machine.js
+++ models/Machine.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose')
 
-// Define a function to format timestamps as "YYYY-MM-DD HH-MM"
-const formatDate = (date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  return `${year}-${month}-${day} ${hours}:${minutes}`;
-};
-
 const machineSchema = mongoose.Schema({
     serial:{
         type: Number,
@@ -32,11 +22,6 @@ const machineSchema = mongoose.Schema({
       type: String,
       required: true
     }
-  },
-  {
-    timestamps: {
-      currentTime: () => Date.now(),
-    },
   }
 );
 
