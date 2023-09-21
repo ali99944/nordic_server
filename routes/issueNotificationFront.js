6db6a93d7d6d39dbc718de549209e5104b2eabf5
ali99944 <ali99944>
2023-09-21 18:42:41 +0300

    upload

--- routes/issueNotificationFront.js
+++ routes/issueNotificationFront.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const IssueNotification = require('../models/IssueNotification')
 
-app.get('/issues/notifications', async (req, res) => {
+router.get('/issues/notifications', async (req, res) => {
     
     const issuenotifications = await IssueNotification.find()
 
     res.render('issueNotifications/index', { issuenotifications: issuenotifications.reverse() });
 });
+
+module.exports = router;
