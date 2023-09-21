757ab6a142483dd74792a9582c6043e91e5a202f
ali99944 <ali99944>
2023-09-21 03:36:39 +0300

    upload

--- routes/issuesRoute.js
+++ routes/issuesRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const IssueNotification = require('../models/IssueNotification')
 const Issue = require('../models/Issue')
+const IssueReport = require('../models/IssueReport');
 const admin = require('../utils/firebase');
 const { sendAlertSMS } = require('../utils/sms_service')
 
@@ -94,6 +95,92 @@ router.post('/issues', async (req, res) => {
                 message: 'Message sent successfully'
              })
 
+    }catch(err){
+        console.log(err.message)
+        return res.status(500).json({message: err.message});
+    }
+})
+
+const multer = require('multer')
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+const path = require('path')
+
+// Set up multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // Set the destination folder where files will be saved
+        cb(null, 'public/images/reports/'); // Create a folder named 'uploads' in your project root
+    },
+    filename: function (req, file, cb) {
+        // Set the file name with original name + timestamp to make it unique
+        cb(null, Date.now() + '_' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/issues/:id/report', upload.single('report') ,async (req, res) => {
+    try{
+        const {
+            details,
+            notes
+        } = req.body
+
+        let image = process.env.BASE_URL + image.path.split('public')[1].replaceAll('\\','/')
+
+        const browser = await puppeteer.launch({
+            headless: 'new',
+            args:['--no-sandbox']
+        });
+        const page = await browser.newPage();
+
+        // Load the HTML template
+        const htmlTemplate = fs.readFileSync('templates/driver.html', 'utf8');
+
+        const now = new Date();
+        const localDate = new Date(now.getTime() + (now.getTimezoneOffset() * 60000));
+        const localDateString = localDate.toISOString().split('T')[0];
+
+        // Replace placeholders with dynamic data
+        const template_data = {
+            details,
+            notes,
+            image,
+            date: localDateString,
+            fullDate: localDate.toDateString(),
+            serial: 'serial',
+            zone: 'zone',
+            zoneLocation: 'zoneLocation'
+        };
+
+        const filledTemplate = Handlebars.compile(htmlTemplate)(template_data);
+
+        let filename = `machine_fix_report_${Date.now()}.pdf`
+
+        // Generate PDF from filled template
+        await page.setContent(filledTemplate);
+        await page.pdf({ path: `./public/profiles/${filename}`,
+        
+        printBackground: true,
+
+        format: 'A3' });
+
+        await browser.close();
+
+        const issueReport = new IssueReport({
+            details: details,
+            notes: notes ?? null,
+            date: localDateString,
+            image: image,
+            pdf: process.env.BASE_URL + 'profiles/' + filename,
+            serial: 'serial',
+            zone: 'zone',
+            zoneLocation: 'zoneLocation'
+        })
+
+        await issueReport.save()
+        return res.status(200).json({ message: 'PDF generated and saved successfully' });
     }catch(err){
         console.log(err.message)
         return res.status(500).json({message: err.message});
