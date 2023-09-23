c7074fae4146518ec0e1eb812608dddc965b7d8c
ali99944 <ali99944>
2023-09-23 04:30:49 +0300

    upload

---
+++ routes/issueReportFront.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const IssueReport = require('../models/IssueReport')
+
+
+router.get('/reports', async (req, res) => {
+  try{
+    let reports = await IssueReport.find()
+    res.status(200).render('reports/index',{
+        reports: reports.reverse()
+    })
+  }catch(err){
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+});
